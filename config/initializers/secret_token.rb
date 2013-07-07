354fc0f8623831fd869845316e249305803c0de3
Ernesto Acevedo <eacevedo@debian.cl>
2013-07-07 10:13:21 -0400

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microcuentos::Application.config.secret_token = '9f9c980bd6b8327649c75459f277090126a54665fb20d27b6c75f20deacf5a3d3970e777246dade9e873c7904036923956eb553db0700778975197f88727a18e'
